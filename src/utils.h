1303caa8cf99160522f95105ba0e9da5844f4700
Rafael <akafael@users.noreply.github.com>
2025-03-14 05:10:31 -0300

    Fix missing function inside .h

--- src/utils.h
+++ src/utils.h
@@ -1,9 +1,14 @@
-#ifndef MY_LIB_H_
-#define MY_LIB_H_
+#ifndef UTILS_H_
+#define UTILS_H_
 
 /**
- * 
+ * Call a super advanced AI engine
  */
 const int deepThought(const char * question);
+
+/**
+ * Return if a given integer number is even
+ */
+const int isEven(const int);
 
 #endif
